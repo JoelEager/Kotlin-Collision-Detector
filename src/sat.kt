2f49a89ce1e8926ec33408fcf6ab58fc4782a44f
Joel Eager <jee7291@rit.edu>
2018-03-23 16:54:21 -0400

    Implemented the SAT algorithm

--- src/sat.kt
+++ src/sat.kt
@@ -24,6 +24,60 @@ fun hasCollided(poly1: Array<Vector>, poly2: Array<Vector>, maxDist: Double?=nul
 }
 
 fun runSAT(poly1: Array<Vector>, poly2: Array<Vector>): Boolean {
+    // Implements the actual SAT algorithm
+    val edges = polyToEdges(poly1) + polyToEdges(poly2)
+    val axes = Array(edges.size, { index -> orthogonal(edges[index])})
 
+    for (axis in axes) {
+        if (!overlap(project(poly1, axis), project(poly2, axis))) {
+            // The polys don't overlap on this axis so they can't be touching
+            return false
+        }
+    }
+
+    // The polys overlap on all axes so they must be touching
+    return true
 }
 
+/**
+ * Returns a vector going from point1 to point2
+ */
+fun edgeVector(point1: Vector, point2: Vector): Vector {
+    return Vector(point2.x - point1.x, point2.y - point1.y)
+}
+
+/**
+ * Returns an array of the edges of the poly as vectors
+ */
+fun polyToEdges(poly: Array<Vector>): Array<Vector> {
+    return Array(poly.size, { index -> edgeVector(poly[index], poly[(index + 1) % poly.size]) })
+}
+
+/**
+ * Returns a new vector which is orthogonal to the given vector
+ */
+fun orthogonal(vector: Vector): Vector {
+    return Vector(vector.y, -vector.x)
+}
+
+/**
+ * Returns the dot (or scalar) product of the two vectors
+ */
+fun dotProduct(vector1: Vector, vector2: Vector): Double {
+    return vector1.x * vector2.x + vector1.y * vector2.y;
+}
+
+/**
+ * Returns a vector showing how much of the poly lies along the axis
+ */
+fun project(poly: Array<Vector>, axis: Vector): Vector {
+    val dots = Array(poly.size, { index -> dotProduct(poly[index], axis) })
+    return Vector(dots.min()!!, dots.max()!!)
+}
+
+/**
+ * Returns a boolean indicating if the two projections overlap
+ */
+fun overlap(projection1: Vector, projection2: Vector): Boolean {
+    return projection1.x <= projection2.y && projection2.x <= projection1.y
+}
